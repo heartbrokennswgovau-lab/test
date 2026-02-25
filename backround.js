dc2becaff7c3b65d8ea6b8bc42e6af4a9fa9a16a
heartbrokennswgovau-lab <heartbroken.nsw.gov.au@gmail.com>
2026-02-25 21:20:10 +1100

    Create backround.js

---
+++ backround.js
@@ -0,0 +1,54 @@
+const URL_MAP = {
+  "||miniblox.*textures/spritesheet.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/spritesheet%20(1).png",
+  "||miniblox.*textures/models/armor/chainmail_layer_1.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/chainmail_layer_1.png",
+  "||miniblox.*textures/models/armor/chainmail_layer_2.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/chainmail_layer_2.png",
+  "||miniblox.*textures/models/armor/diamond_layer_1.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/diamond_layer_1.png",
+  "||miniblox.*textures/models/armor/diamond_layer_2.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/diamond_layer_2.png",
+  "||miniblox.*textures/models/armor/emerald_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/emerald_layer_1.png",
+  "||miniblox.*textures/models/armor/emerald_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/emerald_layer_2.png",
+  "||miniblox.*textures/models/armor/infernium_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/infernium_layer_1.png",
+  "||miniblox.*textures/models/armor/infernium_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/infernium_layer_2.png",
+  "||miniblox.*textures/models/armor/gold_layer_1.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/gold_layer_1.png",
+  "||miniblox.*textures/models/armor/gold_layer_2.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/gold_layer_2.png",
+  "||miniblox.*textures/models/armor/iron_layer_1.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/iron_layer_1.png",
+  "||miniblox.*textures/models/armor/iron_layer_2.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/iron_layer_2.png",
+  "||miniblox.*textures/models/armor/leather_layer_1.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/leather_layer_1.png",
+  "||miniblox.*textures/models/armor/leather_layer_1_overlay.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/leather_layer_1_overlay.png",
+  "||miniblox.*textures/models/armor/leather_layer_2.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/leather_layer_2.png",
+  "||miniblox.*textures/models/armor/leather_layer_2_overlay.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/refs/heads/main/armor/leather_layer_2_overlay.png",
+  "||miniblox.*textures/misc/enchanted_item_glint.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/misc/enchanted_item_glint.png",
+  "||miniblox.*textures/entity/chicken/chicken.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/chicken.png",
+  "||miniblox.*textures/entity/cow/cow.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/cow.png",
+  "||miniblox.*textures/entity/creeper/creeper.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/creeper.png",
+  "||miniblox.*textures/entity/pig/pig.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/pig.png",
+  "||miniblox.*textures/entity/sheep/sheep.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/sheep/sheep.png",
+  "||miniblox.*textures/entity/sheep/sheep_fur.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/sheep/sheep_fur.png",
+  "||miniblox.*textures/entity/skeleton/skeleton.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/skeleton.png",
+  "||miniblox.*textures/entity/slime/slime.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/slime.png",
+  "||miniblox.*textures/entity/spider/spider.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/spider.png",
+  "||miniblox.*textures/entity/spider/spider_eyes.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/spider_eyes.png",
+  "||miniblox.*textures/entity/zombie/zombie.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/zombie.png",
+  "||miniblox.*textures/entity/zombie_cowman/zombie_cowman.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/zombie_cowman.png",
+  "||miniblox.*textures/entity/snowman/snowman.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/snowman.png",
+  "||miniblox.*textures/entity/ghost/ghost.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/ghast/ghast.png",
+  "||miniblox.*textures/entity/ghost/ghost_shooting.png": "https://raw.githubusercontent.com/notsenpai52013-bit/Not_Mini-200-Subs-Texture-Pack/main/entity/ghast/ghast_shooting.png"
+};
+
+let rules = [];
+let idx = 1;
+
+for (const [src, dst] of Object.entries(URL_MAP)) {
+  rules.push({
+    "id": idx++,
+    "action": { "type": "redirect", "redirect": { "url": dst } },
+    "condition": { "urlFilter": src, "resourceTypes": ["image"] }
+  });
+}
+
+chrome.declarativeNetRequest.updateDynamicRules(
+  { addRules: rules, removeRuleIds: rules.map(rule => rule.id) },
+  () => {
+    if (chrome.runtime.lastError) console.error("Error updating:", chrome.runtime.lastError);
+    else console.log("All tool textures applied successfully!");
+  }
+);
